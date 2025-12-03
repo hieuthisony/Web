e680c6424b5c42fae73c68b85b004102684c488a
hieuthisony <hieusony28032004@gmail.com>
2025-12-03 20:31:48 +0700

    init Lab1

---
+++ Lab1/Activity2/Program.cs
@@ -0,0 +1,5 @@
+//Console.WriteLine("Hello World!");
+//Console.WriteLine("Congratulations!");
+//Console.Write("You wrote your first lines of code.");
+Console.WriteLine("This is the first line.");
+Console.Write("This is the second line.");
