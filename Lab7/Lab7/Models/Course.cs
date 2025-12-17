36010236c5d506d94b530cbfb7d12daaf638d6c9
hieuthisony <hieusony28032004@gmail.com>
2025-12-17 20:08:22 +0700

    init Lab5, Lab7

---
+++ Lab7/Lab7/Models/Course.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Lab7.Models
+{
+    public class Course
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.None)] // Cho phép nhập ID thủ công
+        public int CourseID { get; set; }
+        public string Title { get; set; }
+        public int Credits { get; set; }
+        public string? Description { get; set; }
+
+        public ICollection<Enrollment> Enrollments { get; set; }
+    }
+}
---
+++ Lab7/Lab7/Models/Student.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Lab7.Models
+{
+    public class Student
+    {
+        public int ID { get; set; }
+        public string LastName { get; set; }
+        public string FirstMidName { get; set; }
+        public DateTime EnrollmentDate { get; set; }
+
+        [Display(Name = "Email Address")]
+        [EmailAddress]
+        public string? EmailAddress { get; set; }
+        public ICollection<Enrollment> Enrollments { get; set; }
+    }
+}
